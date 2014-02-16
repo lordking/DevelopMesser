eabba432808629a988320c2e1aa47598281224d9
lordking <lordking@163.com>
2014-02-16 11:21:40 +0800

    TabBar

---
+++ TabBarControl.h
@@ -0,0 +1,13 @@
+//
+//  TabBarControl.h
+//  
+//
+//  Created by 金磊 on 14-2-16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarControl : UIControl
+
+@end
---
+++ TabBarControl.m
@@ -0,0 +1,31 @@
+//
+//  TabBarControl.m
+//  
+//
+//  Created by 金磊 on 14-2-16.
+//
+//
+
+#import "TabBarControl.h"
+
+@implementation TabBarControl
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
